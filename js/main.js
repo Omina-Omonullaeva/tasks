a69fdfdae2a03d30663e36b1c272d5810801dcaf
Omina-Omonullaeva <aminaamanullaeva@gmail.com>
2023-10-06 22:23:09 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,49 @@
+// 1-MASALA
+function hello() {
+	return "hello edabit.com" 
+}
+
+// 2-MASALA
+function howManySeconds(hours) {
+	return hours * 3600
+}
+
+// 3-MASALA
+function addition(num) {
+	return num + 1
+}
+
+// 4-MASALA
+function addition(a, b) {
+	return a + b
+}
+
+// 5-MASALA
+function triArea(base, height) {
+	return (base * height) / 2
+}
+
+// 6-MASALA
+function cubes(a) {
+	return a ** 3
+}
+
+// 7-MASALA
+function getFirstValue(arr) {
+	return arr[0]
+}
+
+// 8-MASALA
+function circuitPower(voltage, current) {
+	return voltage * current
+}
+
+// 9-MASALA
+function calcAge(age) {
+	return age * 365
+}
+
+// 10-MASALA
+function points(twoPointers, threePointers) {
+	return twoPointers*2 + threePointers*3;
+}
